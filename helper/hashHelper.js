c724b169d2226fa106709e67475b7b051d2a8f24
Pranjal Tiwari <pranjalt363@gmail.com>
2024-12-24 13:26:16 +0530

    asdfghjk

--- helper/hashHelper.js
+++ helper/hashHelper.js
@@ -6,7 +6,7 @@ const hashPassword = async (password) => {
   const salt = await bcrypt.genSalt(10);
   return await bcrypt.hash(password, salt);
 };
-
+  
 // Function to compare passwords
 const comparePassword = async (candidatePassword, hashedPassword) => {
   return await bcrypt.compare(candidatePassword, hashedPassword);
