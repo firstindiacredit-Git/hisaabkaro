adc848246ce4ec74addcaf711d81488c66c8a1f8
Pranjal Tiwari <pranjalt363@gmail.com>
2024-12-26 13:44:34 +0530

    updates

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
+ 
 //port
 const PORT = 5100 || process.env.PORT;
 
