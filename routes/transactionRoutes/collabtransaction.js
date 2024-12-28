27a61a6610d059f23a38112b9e0599d57c6dda83
Pranjal tiwari <pranjalt363@gmail.com>
2024-12-28 19:10:07 +0530

    work form home

--- routes/transactionRoutes/collabtransaction.js
+++ routes/transactionRoutes/collabtransaction.js
@@ -11,12 +11,17 @@ const {
   deleteTransactionEntry,
 } = require("../../controllers/collaborativeBookController/collaborativeBookController"); // Adjust path if necessary
 
-const authenticate = require("../../middleware/authMiddleware")
+const authenticate = require("../../middleware/authMiddleware");
 const upload = require("../../middleware/uploadMiddleware");
 // Route to create a new transaction
-router.post("/create-transactions",authenticate, upload.single('file'),createTransaction);
+router.post(
+  "/create-transactions",
+  authenticate,
+  upload.single("file"),
+  createTransaction
+);
 // Route to fetch transactions for a user or client
-router.get("/transactions",authenticate, getTransactions);
+router.get("/transactions", authenticate, getTransactions);
 router.get("/client-transactions", authenticate, getTransactionstoclient);
 router.get("/single-transaction/:id", authenticate, getTransactionById);
 // Route to confirm a pending transaction
@@ -28,16 +33,20 @@ router.patch(
 router.patch(
   "/transactions/:transactionId/entries/:entryId",
   authenticate, // Middleware to ensure the user is authenticated
-  upload.single('file'), // Middleware to handle file uploads
+  upload.single("file"), // Middleware to handle file uploads
   updateTransaction // Controller to handle the update logic
 );
 
 router.delete(
   "/transactions/:transactionId/entries/:entryId",
-  authenticate ,  
-  deleteTransactionEntry 
+  authenticate,
+  deleteTransactionEntry
 );
-router.post("/transactions/:transactionId/add",authenticate,upload.single('file'), addExistingTransaction);
+router.post(
+  "/transactions/:transactionId/add",
+  authenticate,
+  upload.single("file"),
+  addExistingTransaction
+);
 
 module.exports = router;
- 
