46c032369c98046b1f73664cacd468edadf32728
firstindiacredit-Git <firsindiacredit786@gmail.com>
2025-04-03 12:03:14 +0530

    new build port5100

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://localhost:3500", process.env.REACT_APP_URI],
+    origin: ["http://:3000", "http://localhost:3500", process.env.REACT_APP_URI],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization", "Accept"],
